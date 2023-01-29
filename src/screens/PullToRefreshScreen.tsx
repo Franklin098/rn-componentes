03331d064a1ba89e60d9c62a24459501d45c7647
Franklin Velasquez <iefranklin@hotmail.es>
2023-01-28 21:04:19 -0600

    add PullToRefresh component

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,53 +1,47 @@
-import React, { useState } from 'react'
-import { View, ScrollView, RefreshControl } from 'react-native'
-import {  } from 'react-native-gesture-handler';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { HeaderTitle } from '../components/HeaderTitle';
-import { styles } from '../theme/appTheme';
+import React, {useState} from 'react';
+import {RefreshControl, ScrollView, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {HeaderTitle} from '../components/HeaderTitle';
+import {styles} from '../theme/appTheme';
 
 export const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [data, setData] = useState<string>('');
+  const {top} = useSafeAreaInsets();
 
-    const { top } =useSafeAreaInsets();
+  const onRefresh = () => {
+    setIsRefreshing(true);
 
-    const [ refreshing, setRefreshing] = useState(false);
-    const [ data, setData] = useState<string>();
+    // mock API call delay
+    setTimeout(() => {
+      console.log('API call completed');
 
-    const onRefresh = () => {
-        setRefreshing(true);
+      setData('Hello World ! - ');
+      setIsRefreshing(false);
+    }, 3000);
+  };
 
-        setTimeout(() => {
-            console.log('Terminamos');
-            setRefreshing(false);
-            setData('Hola mundo')
-        }, 1500);
-    }
+  return (
+    <ScrollView
+      style={{
+        marginTop: isRefreshing ? top : 0,
+      }}
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          // for Android - marginTop offset
+          progressViewOffset={10}
+          style={{backgroundColor: '#5856D6'}}
+          title="Refreshing"
+          titleColor="white"
+        />
+      }>
+      <View style={styles.globalMargin}>
+        <HeaderTitle title="Pull to Refresh" />
 
-    return (
-        <ScrollView
-            style={{
-                marginTop: refreshing ? top : 0
-            }}
-            refreshControl={
-                <RefreshControl 
-                    refreshing={ refreshing }
-                    onRefresh={ onRefresh }
-                    progressViewOffset={ 10 }
-                    progressBackgroundColor="#5856D6"
-                    colors={ ['white','red','orange'] }
-                    // style={{ backgroundColor: '#5856D6' }}
-                    // tintColor="white"
-                />
-            }
-        >
-            <View style={ styles.globalMargin }>
-                <HeaderTitle title="Pull to refresh" />
-
-                {
-                    data && <HeaderTitle title={ data } />
-                }
-                
-                
-            </View>
-        </ScrollView>
-    )
-}
+        <HeaderTitle title={data} />
+      </View>
+    </ScrollView>
+  );
+};
